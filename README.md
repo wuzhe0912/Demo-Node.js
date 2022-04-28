be08de9f1a703ac3497277195fb93ad4cda0c82f
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-04-28 16:46:15 +0800

    chore: redux example initial

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Example Folder
-
-> As example for [obsidian slip box](https://github.com/wuzhe0912/obsidian-slip-box)
+# Redux
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className='App'>Hello Redux Toolkit</div>;
+}
+
+export default App;
